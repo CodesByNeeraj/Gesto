95f196594a611cefc2dc0452dc031968f3e77c5d
CodesByNeeraj <neerajlaks@gmail.com>
2026-07-18 20:25:24 +0800

    fix(actions): flag mac browser tab events

--- src/tab-control.swift
+++ src/tab-control.swift
@@ -1,24 +1,22 @@
 import AppKit
 
-let commandKey: CGKeyCode = 55
-let optionKey: CGKeyCode = 58
 let rightArrowKey: CGKeyCode = 124
 let leftArrowKey: CGKeyCode = 123
 let isPreviousTab = CommandLine.arguments.contains("--previous")
+let navigationModifiers: CGEventFlags = [.maskCommand, .maskAlternate]
 
 func postKey(_ key: CGKeyCode, _ isDown: Bool) {
-    let event = CGEvent(
+    guard let event = CGEvent(
         keyboardEventSource: nil,
         virtualKey: key,
         keyDown: isDown
-    )
-    event?.post(tap: .cghidEventTap)
+    ) else {
+        return
+    }
+    event.flags = navigationModifiers
+    event.post(tap: .cghidEventTap)
 }
 
-postKey(commandKey, true)
-postKey(optionKey, true)
 let tabDirectionKey = isPreviousTab ? leftArrowKey : rightArrowKey
 postKey(tabDirectionKey, true)
 postKey(tabDirectionKey, false)
-postKey(optionKey, false)
-postKey(commandKey, false)
